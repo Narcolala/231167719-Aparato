85dc3faa1000e9bf1adca5606266723ace650370
M303User <M303User@m303.uc-bcf.edu.ph>
2025-03-06 16:05:11 +0800

    First Exercise Finished

---
+++ PhpExercise/Exercise.php
@@ -0,0 +1,14 @@
+<?php
+$a = 15;
+$b = 7;
+
+$sum = $a + $b;
+$difference = $a - $b;
+$product = $a * $b;
+$quotient = $a / $b;
+
+echo "Sum: " . $sum . "\n";
+echo "Difference: " . $difference . "\n";
+echo "Product: " . $product . "\n";
+echo "Quotient: " . $quotient . "\n";
+?>
